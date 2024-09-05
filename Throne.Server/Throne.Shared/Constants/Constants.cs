ef7fc5ce836b5fe5c4c32cc890cb0329e98a21eb
Matheus Reis <matheuss_reis@outlook.com>
2024-09-05 19:54:55 -0300

    Create Constants.cs

---
+++ Throne.Server/Throne.Shared/Constants/Constants.cs
@@ -0,0 +1,12 @@
+namespace Throne.Shared.Constants;
+
+public class Constants
+{
+  public const int MajorVersion = 1;
+  public const int MinorVersion = 0;
+  public const int PatchVersion = 0;
+  public const string ServerHost = "0.0.0.0";
+  public const string ClientHost = "127.0.0.1";
+  public const int Port = 8081;
+  public const int MaxConnections = 1000;
+}
