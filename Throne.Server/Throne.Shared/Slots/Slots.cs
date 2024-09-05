626ed2d530486fbf1c3acc4789c1f50b56e743b5
Matheus Reis <matheuss_reis@outlook.com>
2024-09-05 19:52:14 -0300

    Create Slots.cs

---
+++ Throne.Server/Throne.Shared/Slots/Slots.cs
@@ -0,0 +1,75 @@
+using System;
+
+namespace Throne.Shared.Slots;
+
+public class Slots<T>(int size)
+{
+  private readonly List<T?> _slots = new List<T?>(new T?[size]);
+
+  private void CheckIndex(int index)
+  {
+    if (index < 0 || index >= _slots.Count)
+    {
+      throw new ArgumentOutOfRangeException(nameof(index), $"Index out of range: {index}");
+    }
+  }
+
+  public T? Get(int index)
+  {
+    CheckIndex(index);
+    return _slots[index];
+  }
+
+  public IEnumerable<int> GetFilledSlots()
+  {
+    for (int i = 0; i < _slots.Count; i++)
+    {
+      if (_slots[i] != null)
+      {
+        yield return i;
+      }
+    }
+  }
+
+  public IEnumerable<int> GetEmptySlots()
+  {
+    for (int i = 0; i < _slots.Count; i++)
+    {
+      if (_slots[i] == null)
+      {
+        yield return i;
+      }
+    }
+  }
+
+  public void Remove(int index)
+  {
+    CheckIndex(index);
+    _slots[index] = default;
+  }
+
+  public int Add(T value)
+  {
+    for (int i = 0; i < _slots.Count; i++)
+    {
+      if (_slots[i] == null)
+      {
+        _slots[i] = value;
+        return i;
+      }
+    }
+    throw new InvalidOperationException("No empty slots available");
+  }
+
+  public int? GetFirstEmptySlot()
+  {
+    int index = _slots.IndexOf(default);
+    return index != -1 ? index : (int?)null;
+  }
+
+  public void Update(int index, T value)
+  {
+    CheckIndex(index);
+    _slots[index] = value;
+  }
+}
