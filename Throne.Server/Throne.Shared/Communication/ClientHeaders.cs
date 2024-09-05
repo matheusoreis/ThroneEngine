2fc59070bf4665b95d13c19b6a3ab4e08c47c5b5
Matheus Reis <matheuss_reis@outlook.com>
2024-09-05 19:52:25 -0300

    Create ClientHeaders.cs

---
+++ Throne.Server/Throne.Shared/Communication/ClientHeaders.cs
@@ -0,0 +1,18 @@
+namespace Throne.Shared.Communication;
+
+public enum ClientHeaders
+{
+  Ping,
+  AccessAccount,
+  CreateAccount,
+  DeleteAccount,
+  RecoverAccount,
+  ListCharacters,
+  CreateCharacter,
+  DeleteCharacter,
+  SelectCharacter,
+  SellCharacter,
+  BuyCharacter,
+  CharactersForSale,
+  ConfirmPurchase
+}
