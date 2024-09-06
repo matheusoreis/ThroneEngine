e9d6aa48c6c33e293d3f4bebfbb0355c1fcb8fe9
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2024-09-06 13:06:30 -0300

    Update MemoryManager.cs

--- Throne.Server/Core/Memory/MemoryManager.cs
+++ Throne.Server/Core/Memory/MemoryManager.cs
@@ -1,10 +1,11 @@
 using Throne.Server.Network;
+using Throne.Shared.Constants;
 using Throne.Shared.Slots;
 
 namespace Throne.Server.Core.Memory
 {
-  public class MemoryManager(Slots<WebSocketConnection> connections)
+  public class MemoryManager()
   {
-    public Slots<WebSocketConnection> Connections { get; } = connections;
+    public Slots<WebSocketConnection> Connections { get; } = new Slots<WebSocketConnection>(Constants.MaxConnections);
   }
 }
