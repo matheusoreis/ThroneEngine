6e9c1c7c2bc34637be0faabfe71de7b07b15f64d
Matheus Reis <matheuss_reis@outlook.com>
2024-09-05 21:19:29 -0300

    Update WebSocketConnection.cs

--- Throne.Server/Throne.Server/Network/WebSocketConnection.cs
+++ Throne.Server/Throne.Server/Network/WebSocketConnection.cs
@@ -1,6 +1,40 @@
+using System.Net.WebSockets;
+using Throne.Server.Communication.Protocol;
+using Throne.Shared.Logger;
+
 namespace Throne.Server.Network;
 
-public class WebSocketConnection
+public class WebSocketConnection(WebSocket webSocket, int id)
 {
+  public WebSocket WebSocket { get; } = webSocket ?? throw new ArgumentNullException(nameof(webSocket));
+  public int Id { get; } = id;
+  private MessageHandler Handler { get; } = new();
 
+  public async Task Close()
+  {
+    if (WebSocket.State == WebSocketState.Open || WebSocket.State == WebSocketState.CloseSent)
+    {
+      try
+      {
+        await WebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing connection", CancellationToken.None);
+        Logger.Info($"Connection with ID {Id} closed successfully.");
+      }
+      catch (Exception ex)
+      {
+        Logger.Error($"Error closing connection with ID {Id}: {ex.Message}");
+      }
+    }
+    else
+    {
+      Logger.Error($"Attempted to close connection with ID {Id}, but WebSocket is not open.");
+    }
+  }
+
+  public bool IsOpen() => WebSocket.State == WebSocketState.Open;
+
+  public void ProcessMessage(byte[] buffer)
+  {
+    var message = new ClientMessage(buffer);
+    Handler.ProcessMessage(this, message);
+  }
 }
