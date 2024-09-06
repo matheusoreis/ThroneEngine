a4b50416590e3b0b99adf96605e6bd91c5c64db8
Matheus Reis <matheuss_reis@outlook.com>
2024-09-05 21:20:22 -0300

    Update MessageHandler.cs

--- Throne.Server/Throne.Server/Network/MessageHandler.cs
+++ Throne.Server/Throne.Server/Network/MessageHandler.cs
@@ -1,6 +1,54 @@
+using Throne.Server.Communication.Incoming;
+using Throne.Server.Communication.Protocol;
+using Throne.Shared.Communication;
+using Throne.Shared.Logger;
+
 namespace Throne.Server.Network;
 
 public class MessageHandler
 {
+  private readonly Dictionary<ClientHeaders, IIncoming> messageHandlers = [];
 
+  public MessageHandler()
+  {
+    InitializeHandlers();
+  }
+
+  public void ProcessMessage(WebSocketConnection connection, ClientMessage message)
+  {
+    if (connection == null) return;
+    if (message == null) return;
+
+    short messageId = message.GetId();
+
+    if (Enum.IsDefined(typeof(ClientHeaders), messageId))
+    {
+      ClientHeaders header = (ClientHeaders)messageId;
+
+      if (messageHandlers.TryGetValue(header, out var handler))
+      {
+        try
+        {
+          handler.Handle(connection, message);
+        }
+        catch (Exception e)
+        {
+          Logger.Error($"Error handling {handler.GetType().Name}: {e}");
+          _ = connection.Close();
+        }
+      }
+      else
+      {
+        Logger.Error($"No handler for id: {header}");
+        _ = connection.Close();
+      }
+    }
+    else
+    {
+      Logger.Error($"Invalid message ID: {messageId}");
+      _ = connection.Close();
+    }
+  }
+
+  private static void InitializeHandlers() { }
 }
