f9288a771c3ec4dbd0c5df7c5b9c1b3b9518a993
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2024-09-06 08:33:09 -0300

    Update Constants.cs

--- Throne.Shared/Constants/Constants.cs
+++ Throne.Shared/Constants/Constants.cs
@@ -5,7 +5,7 @@ public class Constants
   public const int MajorVersion = 1;
   public const int MinorVersion = 0;
   public const int PatchVersion = 0;
-  public const string ServerHost = "127.0.0.1";
+  public const string ServerHost = "*";
   public const string ClientHost = "127.0.0.1";
   public const int Port = 8081;
   public const int MaxConnections = 1000;
