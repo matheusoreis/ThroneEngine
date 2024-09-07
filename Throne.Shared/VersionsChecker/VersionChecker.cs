b43d40c8687cb044f31df80df35f8e92d838c334
Matheus Reis <matheuss_reis@outlook.com>
2024-09-07 14:12:39 -0300

    Update VersionChecker.cs

--- Throne.Shared/VersionsChecker/VersionChecker.cs
+++ Throne.Shared/VersionsChecker/VersionChecker.cs
@@ -1,14 +1,24 @@
 namespace Throne.Shared.VersionsChecker;
 
-public class VersionChecker(int expectedMajor, int expectedMinor, int expectedRevision)
+public class VersionChecker
 {
-  private readonly int expectedMajor = expectedMajor;
-  private readonly int expectedMinor = expectedMinor;
-  private readonly int expectedRevision = expectedRevision;
+  private readonly int expectedMajor;
+  private readonly int expectedMinor;
+  private readonly int expectedPatch;
 
-  public bool CheckAndAlert(int major, int minor, int revision)
+  public VersionChecker(int expectedMajor, int expectedMinor, int expectedPatch)
   {
-    if (major != expectedMajor || minor != expectedMinor || revision != expectedRevision) return false;
+    this.expectedMajor = expectedMajor;
+    this.expectedMinor = expectedMinor;
+    this.expectedPatch = expectedPatch;
+  }
+
+  public bool Check(int major, int minor, int revision)
+  {
+    if (major != expectedMajor || minor != expectedMinor || revision != expectedPatch)
+    {
+      return false;
+    }
 
     return true;
   }
